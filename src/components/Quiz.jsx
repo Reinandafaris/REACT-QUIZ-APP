a78f444bf9130475e7795449b27d5af158f6c1f5
Reinandafaris <reinandafaris7@gmail.com>
2024-04-24 20:23:49 +0700

    Mapping Question data

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import QUESTION from "../question";
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
+  const activeQuestionIndex = userAnswers.length;
+
   return (
-    <>
-      <p>Kuis yang sedang dikerjakan</p>
-    </>
+    <div id="question">
+      <p>{QUESTION[activeQuestionIndex].text}</p>
+      <ul id="answers">
+        {QUESTION[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button>{answer}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
